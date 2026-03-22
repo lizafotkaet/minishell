3b36bcf1f9329a999573b6deb06adba9eda32367
lizafotkaet <elizavetabarbashh@gmail.com>
2026-03-22 18:04:42 +0100

    tab indentation in accordance with the fucking Norm in quotes.h

--- include/quotes.h
+++ include/quotes.h
@@ -6,7 +6,7 @@
 /*   By: liza <liza@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/21 04:17:38 by liza              #+#    #+#             */
-/*   Updated: 2026/03/22 18:01:44 by liza             ###   ########.fr       */
+/*   Updated: 2026/03/22 18:04:05 by liza             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,10 @@ typedef struct s_result_t_const_char_ptr
 	t_const_char_ptr	value;
 }	t_result_t_const_char_ptr;
 
-bool				character_is_escaped(const char *start, const char *pos);
+bool						character_is_escaped(const char *start, const char *pos);
 t_result_t_const_char_ptr	closing_double_quote_position(
-					const char *quote_position);
+								const char *quote_position);
 t_result_t_const_char_ptr	closing_single_quote_position(
-					const char *quote_position);
+								const char *quote_position);
 
 #endif
