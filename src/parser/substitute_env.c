ba5c67528a9c69d1284495d5873aaa99112b9511
Elizaveta Barbash <ebarbash@f0r5s17.codam.nl>
2026-03-22 20:09:04 +0100

    refactored substitute_env function in substitute_env.c, created a new file process_env_quotes for too many functions in the former file, norminetted the header files, added the prototypes of now non static functions from both files to the header files; norminetted some other header files

--- src/parser/substitute_env.c
+++ src/parser/substitute_env.c
@@ -6,88 +6,11 @@
 /*   By: ebarbash <ebarbash@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/19 13:03:18 by liza              #+#    #+#             */
-/*   Updated: 2026/03/22 19:39:24 by ebarbash         ###   ########.fr       */
+/*   Updated: 2026/03/22 20:03:02 by ebarbash         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "env.h"
-#include "quotes.h"
-#include "buffer.h"
-#include "substitute_env_internal.h"
-#include "libft.h"
-
-/*
-** Process a single-quoted segment: copy everything literally
-** until closing quote.  Returns pointer past the closing quote,
-** or ERROR on unterminated quote.
-*/
-
-static t_result_t_const_char_ptr	process_single_quoted(const char *it,
-		t_buffer *buf)
-{
-	t_result_t_const_char_ptr	close;
-
-	close = closing_single_quote_position(it);
-	if (close.is_error)
-		return (((t_result_t_const_char_ptr){.is_error = true}));
-	it++;
-	while (it < close.value)
-	{
-		if (!m_buffer_write(buf, it, 1))
-			return (((t_result_t_const_char_ptr){.is_error = true}));
-		it++;
-	}
-	return (((t_result_t_const_char_ptr){.is_error = false, .value = (close.value + 1)}));
-}
-
-/*
-** Process a double-quoted segment: expand $VAR, handle \" \\ \$,
-** copy everything else literally.  Uses closing_double_quote_position
-** to find the end, then processes content up to that point.
-** Returns pointer past the closing quote, or ERROR on unterminated.
-*/
-
-static t_result_t_const_char_ptr	process_double_quoted(const char *it,
-		t_env env, t_buffer *buf)
-{
-	t_result_t_const_char_ptr	close;
-	const char					*end;
-
-	close = closing_double_quote_position(it);
-	if (close.is_error)
-		return (((t_result_t_const_char_ptr){.is_error = true}));
-	end = close.value;
-	it++;
-	while (it < end)
-	{
-		it = expand_char_within_double_quotes(it, end, env, buf);
-		if (it == NULL)
-			return (((t_result_t_const_char_ptr){.is_error = true}));
-	}
-	return (((t_result_t_const_char_ptr){.is_error = false, .value = (end + 1)}));
-}
-
-static t_result_t_const_char_ptr	process_unquoted_char(const char *it,
-		t_env env, t_buffer *buf)
-{
-	if (*it == '\\' && it[1] != '\0')
-	{
-		it++;
-		if (!m_buffer_write(buf, it, 1))
-			return (((t_result_t_const_char_ptr){.is_error = true}));
-		return (((t_result_t_const_char_ptr){.is_error = false, .value = (it + 1)}));
-	}
-	if (*it == '$')
-	{
-		it = expand_variable(it, env, buf);
-		if (it == NULL)
-			return (((t_result_t_const_char_ptr){.is_error = true}));
-		return (((t_result_t_const_char_ptr){.is_error = false, .value = (it)}));
-	}
-	if (!m_buffer_write(buf, it, 1))
-		return (((t_result_t_const_char_ptr){.is_error = true}));
-	return (((t_result_t_const_char_ptr){.is_error = false, .value = (it + 1)}));
-}
+#include "minishell.h"
 
 /*
 ** Process unquoted text: expand $VAR, handle backslash escapes,
@@ -95,34 +18,17 @@ static t_result_t_const_char_ptr	process_unquoted_char(const char *it,
 ** Returns pointer past the processed text, or ERROR on unterminated quote.
 */
 
-static t_result_t_const_char_ptr	substitute_env_with_buffer(const char *it,
+t_result_t_const_char_ptr	substitute_env_with_buffer(const char *it,
 		t_env env, t_buffer *buf)
 {
 	t_result_t_const_char_ptr	r;
 
 	while (*it != '\0')
 	{
-		if (*it == '\'')
-		{
-			r = process_single_quoted(it, buf);
-			if (r.is_error)
-				return (((t_result_t_const_char_ptr){.is_error = true}));
-			it = r.value;
-		}
-		else if (*it == '"')
-		{
-			r = process_double_quoted(it, env, buf);
-			if (r.is_error)
-				return (((t_result_t_const_char_ptr){.is_error = true}));
-			it = r.value;
-		}
-		else
-		{
-			r = process_unquoted_char(it, env, buf);
-			if (r.is_error)
-				return (((t_result_t_const_char_ptr){.is_error = true}));
-			it = r.value;
-		}
+		r = process_next_segment(it, env, buf);
+		if (r.is_error)
+			return (((t_result_t_const_char_ptr){.is_error = true}));
+		it = r.value;
 	}
 	return (((t_result_t_const_char_ptr){.is_error = false, .value = (it)}));
 }
